ce316ac723914dac6652d08e132c39e82d4dbb95
d4rkFox <maks.petri4enko@yandex.ru>
2020-10-15 19:30:04 +0700

    14 practice cycle correct

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,77 @@
 "use strict";
+let check, nameFilm;
+
 for (let i = 0; i < 1; i++) {
     const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-    console.log(numberOfFilms);
-    const check = +numberOfFilms;
-    console.log(typeof check);
-    console.log(isNaN(check));
-    const nameFilm = numberOfFilms.length;
-    if (
-        nameFilm == 0 ||
-        nameFilm > 50 ||
-        isNaN(check) === true ||
-        numberOfFilms === null
-    ) {
+    if (numberOfFilms === null) {
         i--;
-        alert("Не верно заполнено, введите заново");
+        alert("Поле обязательное к заполнению");
     } else {
-        const personalMovieDB = {
-            count: numberOfFilms,
-            movies: {},
-            actors: {},
-            genres: [],
-            privat: false,
-        };
-        for (let j = 0; j <= 1; j++) {
-            let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-            const lastFilmName = lastFilm.length;
-            if (lastFilmName == 0 || lastFilmName > 50 || lastFilm === null) {
-                j = 0;
-                alert("Не верно заполнено, введите заново");
-            } else {
-                for (let k = 0; k <= 1; k++) {
-                    let filmRate = prompt("На сколько оцените его?","например 9.5");
-                    const rateLenght = filmRate.length;
-                    const secondCheck = +filmRate;
-                    if (rateLenght == 0 || rateLenght > 50 || isNaN(secondCheck) === true || filmRate === null) {
-                        k = 0;
+        check = +numberOfFilms;
+        nameFilm = numberOfFilms.length;
+        if (nameFilm == 0 || nameFilm > 50 || isNaN(check) === true) {
+            i--;
+            alert("Не верно заполнено, введите заново");
+        } else {
+            const personalMovieDB = {
+                count: numberOfFilms,
+                movies: {},
+                actors: {},
+                genres: [],
+                privat: false,
+            };
+            let lastFilmName;
+            for (let j = 0; j <= 1; j++) {
+                let lastFilm = prompt("Один из последних просмотренных фильмов?","");
+                if (lastFilm === null) {
+                    j--;
+                    alert("Поле обязательное к заполнению");
+                } else {
+                    lastFilmName = lastFilm.length;
+                    if (lastFilmName == 0 || lastFilmName > 50) {
+                        j--;
                         alert("Не верно заполнено, введите заново");
                     } else {
-                        personalMovieDB.movies[lastFilm] = filmRate;
-               
-                        k = 2;
+                        let rateLenght, secondCheck;
+
+                        for (let k = 0; k <= 1; k++) {
+                            let filmRate = prompt(
+                                "На сколько оцените его?",
+                                "например 9.5"
+                            );
+                            if (filmRate === null) {
+                                k = 0;
+                                alert("Поле обязательное к заполнению");
+                            } else {
+                                rateLenght = filmRate.length;
+                                secondCheck = +filmRate;
+                                if (
+                                    rateLenght == 0 ||
+                                    rateLenght > 50 ||
+                                    isNaN(secondCheck) === true
+                                ) {
+                                    k = 0;
+                                    alert("Не верно заполнено, введите заново");
+                                } else {
+                                    personalMovieDB.movies[lastFilm] = filmRate;
+                                    k = 2;
+                                }
+                            }
+                        }
                     }
                 }
             }
+            if (personalMovieDB.count < 10) {
+                alert("Просмотрено довольно мало фильмов");
+            } else if (
+                personalMovieDB.count > 10 &&
+                personalMovieDB.count < 30
+            ) {
+                alert("Вы классический зритель");
+            } else {
+                alert("Вы киноман");
+            }
+            console.log(personalMovieDB);
         }
-        if (personalMovieDB.count < 10) {
-            alert("Просмотрено довольно мало фильмов");
-        } else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-            alert("Вы классический зритель");
-        } else {
-            alert("Вы киноман");
-        }
-        console.log(personalMovieDB);
     }
 }
